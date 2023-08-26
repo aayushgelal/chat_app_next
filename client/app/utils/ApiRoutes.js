bc87f49f53986aa9f13de58a0d0c91a7f49e1939
aayushgelal <aayushgelal@gmail.com>
2023-08-26 10:20:33 +0545

    final

--- client/app/utils/ApiRoutes.js
+++ client/app/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const Host = "hthttps://chat-app-server-yc1z.onrender.com";
+export const Host = "https://chat-app-server-yc1z.onrender.com";
 const AUTH_ROUTE = `${Host}/auth`;
 export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/login`;
 export const SIGNUP_USER_ROUTE = `${AUTH_ROUTE}/signup`;
